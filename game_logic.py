b1d81d2c757d9d032a9d89b3fb9293bee5746483
kugkrisz88 <krisztofer.kugler88@gmail.com>
2024-04-18 20:56:21 +0200

    Commit 4: Define all functions based on commented pseudo text to create a map what the program should do. Implement main entry in the main.py that runs the main() and the imported Game class after that only if opened from the main.py

--- game_logic.py
+++ game_logic.py
@@ -1,5 +1,17 @@
 class Game:
+    #Initialize the current condition of the game (default)
     def __init__(self):
         self.word = ""
         self.guesses = set()
         self.attempts = 6
+    #A pool of random words and function to get a random word for the current game
+    def get_random_word(self):
+    #A method to display the word with hidden letters (-)
+    def display_word(self):
+    #Check if the guess is correct, if not take one attempt away
+    def check_guess(self, guess):
+    #A method for checking game over conditions (The number of your attemps are 0 or you guessed the word)
+    def is_game_over(self):
+    #Runs the game loop till the gameover function is true
+    def run(self):
+    print("Welcome to Hangman!")
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from game_logic import Game
+#Handle game initialization within the main function
+def main():
+    game = Game()
+    game.run()
+
+#Ensures that main.py is the main entry and it runs directly from the Python interpreter
+if __name__ == "__main__":
+    main()
