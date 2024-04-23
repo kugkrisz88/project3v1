c7d2b86c72cfb47544c7619ddd6a7716e7fdd0d3
kugkrisz88 <krisztofer.kugler88@gmail.com>
2024-04-23 22:02:47 +0200

    Deployment Commit 3 : Try configuring the run.py to add port manually

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
+import os
 from game_logic import Game
+
 #Handle game initialization within the main function
 def main():
     game = Game()
     game.run()
 
-#Ensures that main.py is the main entry and it runs directly from the Python interpreter
+#Ensures that run.py is the main entry and it runs directly from the Python interpreter
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
